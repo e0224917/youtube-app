258b87a541791ed11f7780f56f159404bc183e50
Nhat Ton <minhnhattonthat@gmail.com>
2018-02-13 16:33:58 +0800

    Initial Commit

---
+++ youtube-appTests/youtube_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  youtube_appTests.swift
+//  youtube-appTests
+//
+//  Created by Nhat Ton on 13/2/18.
+//  Copyright © 2018 Nhat Ton. All rights reserved.
+//
+
+import XCTest
+@testable import youtube_app
+
+class youtube_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
